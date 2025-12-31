de4d308e401ce867e85235a14246f605a57f6f78
caironightvip <nightclub2026@gmail.com>
2025-12-31 15:10:38 +0200

    012 WhatsAppButton.tsx

--- src/components/WhatsAppButton.tsx
+++ src/components/WhatsAppButton.tsx
@@ -13,7 +13,7 @@ interface WhatsAppButtonProps {
 }
 
 const WhatsAppButton = ({
-  phoneNumber = "201055599706",
+  phoneNumber = "201286110562",
   message = "مرحباً، أود معرفة تفاصيل الحجز والأسعار في نايت كلوب ?",
   position = "bottom-right",
   showTooltip = true,
